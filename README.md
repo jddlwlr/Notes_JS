29926e3c5d9610516055f93ab779f6fdadc35a67
Jesse Lawler <jedd.lawler@gmail.com>
2022-01-22 07:37:37 -0700

    working on heroku deployment

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Notes_JS
+
+# **Note Taker** 
+
+## Synopsis
+
+* Save notes!
+* Saves user text area input to the database for later retrieval
+* Delete notes! 
+* Has it's own server! 
+
+**See it Live:** 
+
+<!-- https://jddlwlr.github.io/workday_schedule_utility/ -->
+
+## Usage
+
+This application is to be used to save notes. 
+<br>
+
+
+![Site overview image](./public/assets/images/SiteImage.png)
+
+### Features
+
+* Express.js server
+* Clean layout/ UI
+* Database containing all notes
+* Live deployment through Heroku
+<br>
+<br>
+
+## Code
+
+This application utilizes Express.js for the server, FS for file edits.  
+
+### Limitations
+
+This application literally has no limits, you can note anything you could possibly imagine. Mom's spaghetti recipe? check. Your favorite links? check. Your memior? check.
+
+## With Thanks to
+
+Google, my discord classmates, and the ever popular MDN. 
+
+## License
+
+Copywritten by me Jesse Lawler (2022)
+
+## About Jesse Lawler
+
+Jesse Lawler is a Geologist turned aspiring web developer currently enrolled in the Georgia institure of Technologies online full-stack web development course. Jesse like fishing, skiing and _long_ walks on the beach. 
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,59 @@ const router = (app) => {
 
     fs.readFile("db/db.json","utf8", (err, data) => {
 
-    })
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+
+        // notes post route
+        app.post("/api/notes", function(req, res) {
+
+            let newNote = req.body;
+            notes.push(newNote);
+            writeDb();
+            
+        });
+
+        // notes get route
+        app.get("/api/notes", function(req, res) {
+            // Read the db.json file and return all saved notes as JSON.
+            res.json(notes);
+        });
+
+
+        // displays notes
+        app.get("/api/notes/:id", function(req,res) {
+            // display json for the notes array indices of the provided id
+            res.json(notes[req.params.id]);
+        });
+
+
+        // sends notes db
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+        
+        // catches all other paths
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        // delete notes
+        app.delete("/api/notes/:id", function(req, res) {
+            notes.splice(req.params.id, 1);
+            writeDb();
+        });
+
+        // re-writes db
+        function writeDb() {
+            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
+    });
+
 }
 
+module.exports = router;
